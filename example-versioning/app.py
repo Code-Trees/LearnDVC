4f711395e825f812bc599b6bd0f5fc77e1a4831b
Pruthiraj Jayasingh <data.pruthiraj@gmail.com>
2023-05-21 08:07:55 +0000

    code added

--- example-versioning/app.py
+++ example-versioning/app.py
@@ -1,19 +1,48 @@
 from flask import Flask, render_template, request
+import torch
+from torchvision import transforms,models
+from PIL import Image
+from torchvision.models import resnet50, ResNet50_Weights
 
 app = Flask(__name__)
 
-# Define a route that will render the HTML file
+# Load the PyTorch model
+model = models.alexnet(pretrained=True)
+model.eval()
+
+# Define the image transformation pipeline
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
+    ])
+
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-# Define a route that will handle the form submission
-@app.route('/', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
+    # Get the image file from the request
     file = request.files['image']
-    # Call your prediction function here, passing the uploaded file as an argument
-    prediction_result = 'Prediction result'
-    return prediction_result
+
+    # Load the image and apply the transformation pipeline
+    img = Image.open(file)
+    img = transform(img)
+    img = img.unsqueeze(0)
+
+    with open('imagenet_class.txt') as f:
+        classes = [line.strip() for line in f.readlines()]
+
+    # Make a prediction using the PyTorch model and return the result
+    with torch.no_grad():
+        output = model(img)
+        _, predicted = torch.max(output, 1)
+        label = predicted.item()
+
+    return render_template('index.html', prediction= str(classes[label]))
 
 if __name__ == '__main__':
     app.run(debug=True)
